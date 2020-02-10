977d01182e3e428ba86a28cbef5fe00cbcc0ea4f
Jin Chen <jinchen93@gmail.com>
2020-02-10 08:30:40 -0800

    Logging

--- SettingsHelper.js
+++ SettingsHelper.js
@@ -26,11 +26,11 @@ module.exports = function () {
   const output = {};
   for (let i = 0; i < fields.length; i++) {
     const setting = fields[i];
-    // console.log('Processing setting', setting)
-    // console.log(args[setting], process.env[setting], defaults[setting])
+    console.log('Processing setting', setting)
+    console.log(args[setting], process.env[setting], defaults[setting])
     output[setting] = args[setting] || process.env[setting] || settings[setting] || defaults[setting];
     process.env[setting] = output[setting];
   }
-  // console.log('Our settings are', output)
+  console.log('Our settings are', output)
   return output;
 };
